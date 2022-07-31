ef9f0fca02cf616c8fae270ab250c97f15fda113
Lichi Liao <lichiliao617@gmail.com>
2022-07-31 19:50:56 +0800

    setup node-red-test-helper-tool

---
+++ src/NodeRedTestServer.js
@@ -0,0 +1,29 @@
+
+export class NodeRedTestServer{
+    server
+    constructor(server){
+        this.server = server
+    }
+    async healthCheck(nodeArray,flow,testNodeId){
+        let result = false
+        await this.server.load(nodeArray,flow,async()=>{
+            const n0 = this.server.getNode(testNodeId)
+            // console.log(n0)
+            result = n0!==null
+        })
+        this.server.unload()
+        return result
+    }
+    async testFlow(nodeArray,flow,inputNodeId,outputNodeId,testInput){
+        let testOutput = undefined
+        await this.server.load(nodeArray,flow,()=>{
+            const inputNode = this.server.getNode(inputNodeId)
+            const outputNode = this.server.getNode(outputNodeId)
+            inputNode.send(testInput)
+            outputNode.on('input',(msg)=>{
+                testOutput = msg
+            })
+        })
+        return testOutput
+    }
+}
